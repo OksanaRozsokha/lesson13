274cff41fd1d1830430a9f8bd0b0ed00f67c6741
Оксана <o.rozsokha@gmail.com>
2017-01-18 01:15:41 +0200

    jq-tab

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+    $('.markers-list > li').on('click', function () {
+        var markerItem = $(this).index();
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+        $('.slider-list .slider-text').slideUp();
+        $('.slider-list .slider-text').eq(markerItem).slideDown();
+    });
+
     var rotateItem = $('.features-list li');
     rotateItem.mouseover(function () {
         $(this).find('.rotate').css('transform','rotate(360deg)');
@@ -8,5 +17,14 @@ $(document).ready(function () {
         $(this).find('.rotate').css('transform','rotate(-360deg)');
     });
 
+    $('.jq-btn').on('click', function () {
+        var pVisible = $('.jq-tab p');
+        $(this).text(pVisible.is(':visible') ? 'view more' : 'show less');
+        $(this).parent().find('p').toggle(300);
+        $(this).parent().find('h2').toggleClass('active-title');
+    });
 
+    $('.title-wrap').on('click', function () {
+        $('.title-wrap').animate({width:'+=1050px'}, 'slow');
+    });
 });
